fd7ba0682caacd42089333d638b5760bebdeb36d
LOKESH KUMAR <lokeshkumarbijnor@gmail.com>
2021-12-19 21:42:20 +0530

    h

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
-#!/Python/python.exe
-print ("Content-type:text/html\r\n\r\n")
+#!C:\Python\python.exe
+
+print('content-type: text/html\r\n\r\n')
+import cgi,os
+
+form = cgi.FieldStorage()
+msg = form.getvalue("message_py")
 print ('<html>')
+print ('<head>')
+print ('<title>Hello World - First CGI Program</title>')
+print ('</head>')
 print ('<body>')
-print ('<h2>Hello World! This is my first CGI program</h2>')
+print ('<h2>Hello World! This is my first CGI program %s</h2>'% msg)
 print ('</body>')
 print ('</html>')
