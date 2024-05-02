0097230af150ec5f374d94f812df5a4abd16e11e
LOKESH KUMAR <lokeshkumarbijnor@gmail.com>
2024-05-02 07:47:08 +0530

    Update st_app.py

--- st_app.py
+++ st_app.py
@@ -3,5 +3,7 @@ import streamlit as st
 
 st.write("Hi Welcome to my page")
 file_name = "https://github.com/Lokeshkumarbijnor/test/blob/main/abc.txt"
+print(file_name)
 with open(file_name, 'r') as f:
+  
   st.write(f.read())
